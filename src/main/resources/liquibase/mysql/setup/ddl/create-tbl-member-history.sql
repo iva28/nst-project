ee095c48d4c2b18fe7fa4b098eb9a0e4ec7392b4
iva28 <iva.hurt574@gmail.com>
2024-05-11 16:01:29 +0200

    changed restriction in fk

--- src/main/resources/liquibase/mysql/setup/ddl/create-tbl-member-history.sql
+++ src/main/resources/liquibase/mysql/setup/ddl/create-tbl-member-history.sql
@@ -7,7 +7,7 @@ CREATE TABLE tbl_member_role_history(
     department_id BIGINT,
     PRIMARY KEY(id),
     CONSTRAINT member_fk2 FOREIGN KEY (member_id) REFERENCES tbl_member(id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
+    ON UPDATE CASCADE ON DELETE RESTRICT,
     CONSTRAINT department_fk2 FOREIGN KEY (department_id) REFERENCES tbl_department(id)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
